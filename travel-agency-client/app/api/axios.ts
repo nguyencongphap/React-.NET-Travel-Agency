731d76a6522286330860b2581b7e6b8210c848a6
PHAP_LAPTOP\ncpha <nguyencongphap@yahoo.com>
2025-06-18 16:36:44 -0700

    Fixed error of not being able to connect to the server

--- travel-agency-client/app/api/axios.ts
+++ travel-agency-client/app/api/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = "https://localhost:7105";
+// const BASE_URL = "https://localhost:7105";
+const BASE_URL = "http://localhost:5198";
 
 export default axios.create({
   baseURL: BASE_URL,
