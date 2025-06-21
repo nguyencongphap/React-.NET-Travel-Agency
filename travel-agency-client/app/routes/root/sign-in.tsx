6e067bfc62a736ca57303c88f83c5d656941347f
PHAP_LAPTOP\ncpha <nguyencongphap@yahoo.com>
2025-06-21 15:15:34 -0700

    Fix error in the redirect logic of sign-in

--- travel-agency-client/app/routes/root/sign-in.tsx
+++ travel-agency-client/app/routes/root/sign-in.tsx
@@ -9,7 +9,7 @@ export async function clientLoader() {
   try {
     const user = await GetCurrentUser();
 
-    if (!user.id) return redirect("/");
+    if (user.id) return redirect("/");
   } catch (error) {
     console.log("Error fetching user", error);
   }
